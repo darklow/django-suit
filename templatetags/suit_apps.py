132fd36500cec0abb4fa9c48bcb42b93abc7448e
Kaspars Sprogis <darklow@gmail.com>
2013-02-09 14:10:31 +0200

    Menu order support added

--- templatetags/suit_apps.py
+++ templatetags/suit_apps.py
@@ -26,6 +26,8 @@ def get_app_list(context, request):
     exclude = suit.get_config('MENU_EXCLUDE')
     parent_link = suit.get_config('MENU_PARENT_LINK')
     icons = suit.get_config('MENU_ICONS')
+    menu_order = suit.get_config('MENU_ORDER')
+
     for app in app_list:
         app_name = app['name'].lower()
 
@@ -34,32 +36,68 @@ def get_app_list(context, request):
             app_list.remove(app)
             continue
 
+        # Set icon if configured
         if icons and app_name in icons:
             app['icon'] = icons[app_name]
 
+        # Mark as active by url or app name match
         if request.path == app['app_url'] or \
                         current_app == app_name:
             app['is_active'] = True
 
+        # Set first child url unless MENU_PARENT_LINK = True
         if not parent_link:
             app['app_url'] = app['models'][0]['admin_url']
 
         # Iterate models
         for model in app['models']:
-            model_url = model['admin_url']
 
-            # Get model name by url last part
             # Exclude if in exclude list
-            model_name = model_url.rstrip('/').split('/')[-1]
-            model_full_name = '%s.%s' % (app_name, model_name)
-
+            model_full_name = '%s.%s' % (app_name, get_model_name(model))
             if exclude and model_full_name in exclude:
                 app['models'].remove(model)
                 continue
 
-            model_name_pl = model['name'].lower()
-            if request.path == model_url or \
-                            curr_model_name_pl == model_name_pl:
+            # Mark as active by url or model plural name match
+            if request.path == model['admin_url'] or \
+                            curr_model_name_pl == model['name'].lower():
                 model['is_active'] = True
 
+    # Reorder menu
+    if menu_order:
+        app_list = reorder_apps(app_list, menu_order)
+
     return app_list
+
+
+def reorder_apps(app_list, menu_order):
+    new_apps = []
+    for order in menu_order:
+        app_name = order[0]
+        models_order = order[1] if len(order) > 1 else None
+        for app in app_list:
+            if app['name'].lower() == app_name:
+                new_apps.append(app)
+                app_list.remove(app)
+                if models_order:
+                    reorder_app_models(app, models_order)
+                break
+
+    return new_apps
+
+
+def reorder_app_models(app, model_order):
+    new_models = []
+    for model_name in model_order:
+        for model in app['models']:
+            if model_name == get_model_name(model):
+                new_models.append(model)
+
+    app['models'] = new_models
+
+
+def get_model_name(model):
+    """
+    Get model name by its last part of url
+    """
+    return model['admin_url'].rstrip('/').split('/')[-1]
