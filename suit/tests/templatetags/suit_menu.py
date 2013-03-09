8ad100edb3977acfde7a078100ac5ce393e88ae0
Kaspars Sprogis <darklow@gmail.com>
2013-03-09 21:24:35 +0200

    Tests: Added menu individual object tests

--- suit/tests/templatetags/suit_menu.py
+++ suit/tests/templatetags/suit_menu.py
@@ -1,11 +1,15 @@
 from django.conf import settings
 from django.core.urlresolvers import reverse
+from django.test.html import parse_html
+from django.utils.encoding import force_unicode
+from suit.templatetags.suit_menu import get_menu
 from suit.tests.mixins import ModelsTestCaseMixin, UserTestCaseMixin
 
 
 class SuitMenuTestCase(ModelsTestCaseMixin, UserTestCaseMixin):
-    def perform_response(self):
-        self.response = self.client.get(reverse('admin:index'))
+    def perform_response(self, url=None):
+        url = url or reverse('admin:index')
+        self.response = self.client.get(url)
 
     def setUp(self):
         # Menu settings
@@ -91,3 +95,75 @@ class SuitMenuTestCase(ModelsTestCaseMixin, UserTestCaseMixin):
         self.assertNotContains(self.response, menu_order[2][0][0])
         # Test custom app when perms defined as tuple
         self.assertNotContains(self.response, menu_order[3][0][0])
+
+    def test_menu_marked_as_active(self):
+        self.perform_response(reverse('admin:app_list', args=['tests']))
+        self.assertContains(self.response, '<li class="active">')
+
+    def make_menu_from_response(self):
+        return get_menu(self.response.context[-1], self.response._request)
+
+    def test_menu_app_marked_as_active(self):
+        self.perform_response(reverse('admin:app_list', args=['tests']))
+        menu = self.make_menu_from_response()
+        self.assertTrue(menu[0]['is_active'])
+
+    def test_menu_model_marked_as_active(self):
+        self.perform_response(reverse('admin:tests_book_changelist'))
+        menu = self.make_menu_from_response()
+        self.assertTrue(menu[0]['is_active'])
+        self.assertTrue(menu[0]['models'][0]['is_active'])
+
+    def test_menu_as_object_from_template_tag(self):
+        self.perform_response(reverse('admin:app_list', args=['tests']))
+        menu = self.make_menu_from_response()
+
+        # Convert translation proxies for models to unicode
+        for app in menu:
+            models = app.get('models', None)
+            if models:
+                for model in models:
+                    model['name'] = force_unicode(model['name'])
+
+        # Todo
+        # Split menu_expected into multiple-parts and compare each part
+        # individually to MENU_ORDER
+        menu_expected = [
+            {'app_url': '/admin/tests/',
+             'name': u'Tests',
+             'models': [
+                 {'perms': {'add': True, 'change': True, 'delete': True},
+                  'add_url': '/admin/tests/book/add/',
+                  'admin_url': '/admin/tests/book/',
+                  'is_active': False,
+                  'name': u'Books'
+                 }
+             ],
+             'is_active': True,
+             'has_module_perms': True,
+             'icon': 'icon-fire icon-test-against-keyword'
+            },
+            {'app_url': '/custom-url-test/',
+             'models': [
+                 {'admin_url': '/admin/custom/',
+                  'is_active': False,
+                  'name': u'Custom link'},
+                 {'admin_url': '/admin/non-existant/',
+                  'is_active': False,
+                  'name': u'Check out error 404'},
+                 {'perms':
+                      {'add': True, 'change': True, 'delete': True},
+                  'add_url': '/admin/tests/album/add/',
+                  'admin_url': '/admin/tests/album/',
+                  'is_active': False,
+                  'name': u'Albums'}],
+             'name': 'Custom app name', 'icon': 'icon-custom-app'},
+            {'app_url': '/custom-app-no-models', 'models': [],
+             'name': 'Custom app no models', 'icon': ''},
+            {'app_url': '/custom-app-tuple-perms',
+             'models': [],
+             'name': 'Custom app no models tuple perms',
+             'icon': ''}
+        ]
+
+        self.assertEqual(menu, menu_expected)
