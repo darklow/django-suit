05b19a9d5c650b29c5cbe80fb8aeb039c87032f1
Kaspars Sprogis <darklow@gmail.com>
2013-03-10 14:17:12 +0200

    Tests: Travis CI settings tweaking and missing manage.py fixed

--- suit/tests/urls.py
+++ suit/tests/urls.py
@@ -1,11 +1,9 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
-from djangosuit import views
 
 admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples for custom menu
-    url(r'^admin/custom/$', views.custom_view),
     url(r'^admin/', include(admin.site.urls)),
 )
