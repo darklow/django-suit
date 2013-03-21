9f2d755b4e6505bd55e1cc5c21a2c92dabe51976
Philipp Hossner <philipp.hossner@gmail.com>
2013-03-21 04:37:08 +0100

    Added basic mapping of fieldsets to tabs

--- suit/admin.py
+++ suit/admin.py
@@ -1,4 +1,6 @@
-import suit.config
+from django.contrib.contenttypes.models import ContentType
+from django.template.response import TemplateResponse
+from django.utils.safestring import mark_safe
 from django.contrib.admin import ModelAdmin
 from django.contrib.admin.views.main import ChangeList
 from django.forms import ModelForm
@@ -112,6 +114,45 @@ class SortableModelAdmin(SortableModelAdminBase, ModelAdmin):
                 next_order = 1
             setattr(obj, self.sortable, next_order)
         super(SortableModelAdmin, self).save_model(request, obj, form, change)
+
+
+class TabbedModelAdmin(ModelAdmin):
+    """
+    The same as ModelAdmin, except the fieldsets are rendered as tabs
+    """
+
+    def render_change_form(self, request, context, add=False, change=False, form_url='', obj=None):
+        """
+        Almost the same method as in the superclass, but the templates are different. Maybe a cleaner solution is possible.
+        """
+        opts = self.model._meta
+        app_label = opts.app_label
+        ordered_objects = opts.get_ordered_objects()
+        context.update({
+            'add': add,
+            'change': change,
+            'has_add_permission': self.has_add_permission(request),
+            'has_change_permission': self.has_change_permission(request, obj),
+            'has_delete_permission': self.has_delete_permission(request, obj),
+            'has_file_field': True,
+            'has_absolute_url': hasattr(self.model, 'get_absolute_url'),
+            'ordered_objects': ordered_objects,
+            'form_url': mark_safe(form_url),
+            'opts': opts,
+            'content_type_id': ContentType.objects.get_for_model(self.model).id,
+            'save_as': self.save_as,
+            'save_on_top': self.save_on_top,
+        })
+        if add and self.add_form_template is not None:
+            form_template = self.add_form_template
+        else:
+            form_template = self.change_form_template
+
+        return TemplateResponse(request, form_template or [
+            "suit/%s/%s/tabbed_change_form.html" % (app_label, opts.object_name.lower()),
+            "suit/%s/tabbed_change_form.html" % app_label,
+            "suit/tabbed_change_form.html"
+        ], context, current_app=self.admin_site.name)
 
 
 
