32d185e7819d157b0e9fb00c557d286f26f1b102
Kaspars Sprogis <darklow@gmail.com>
2013-12-28 10:06:48 +0200

    Docs: Add try/except for new docs theme, otherwise build on readthedocs.org fails

--- docs/conf.py
+++ docs/conf.py
@@ -12,8 +12,14 @@
 # serve to show the default.
 
 import sys, os
-import sphinx_rtd_theme
 
+try:
+    import sphinx_rtd_theme
+
+    html_theme = 'sphinx_rtd_theme'
+    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+except ImportError:
+    html_theme = 'default'
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
@@ -93,7 +99,7 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'sphinx_rtd_theme'
+# html_theme = 'sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -102,7 +108,7 @@ html_theme = 'sphinx_rtd_theme'
 
 # Add any paths that contain custom themes here, relative to this directory.
 #html_theme_path = []
-html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+# html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
@@ -173,21 +179,21 @@ htmlhelp_basename = 'DjangoSuitdoc'
 # -- Options for LaTeX output --------------------------------------------------
 
 latex_elements = {
-# The paper size ('letterpaper' or 'a4paper').
-#'papersize': 'letterpaper',
+    # The paper size ('letterpaper' or 'a4paper').
+    #'papersize': 'letterpaper',
 
-# The font size ('10pt', '11pt' or '12pt').
-#'pointsize': '10pt',
+    # The font size ('10pt', '11pt' or '12pt').
+    #'pointsize': '10pt',
 
-# Additional stuff for the LaTeX preamble.
-#'preamble': '',
+    # Additional stuff for the LaTeX preamble.
+    #'preamble': '',
 }
 
 # Grouping the document tree into LaTeX files. List of tuples
 # (source start file, target name, title, author, documentclass [howto/manual]).
 latex_documents = [
-  ('index', 'DjangoSuit.tex', u'Django Suit Documentation',
-   u'Kaspars Sprogis (darklow)', 'manual'),
+    ('index', 'DjangoSuit.tex', u'Django Suit Documentation',
+     u'Kaspars Sprogis (darklow)', 'manual'),
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
@@ -230,9 +236,10 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-  ('index', 'DjangoSuit', u'Django Suit Documentation',
-   u'Kaspars Sprogis (darklow)', 'DjangoSuit', 'One line description of project.',
-   'Miscellaneous'),
+    ('index', 'DjangoSuit', u'Django Suit Documentation',
+     u'Kaspars Sprogis (darklow)', 'DjangoSuit',
+     'One line description of project.',
+     'Miscellaneous'),
 ]
 
 # Documents to append as an appendix to all manuals.
