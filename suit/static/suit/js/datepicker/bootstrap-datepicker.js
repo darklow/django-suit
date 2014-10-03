c7b8c14f2138dd0428858a4131476540e8b25b58
Kaspars Sprogis <darklow@gmail.com>
2014-10-03 13:09:12 +0300

    Fixed bug when datepicker is cleared on second click Base on this commit: https://github.com/tcrossland/bootstrap-datepicker/commit/f86114f515806871692aec2cf7cbbcb66a8a5f21

--- suit/static/suit/js/datepicker/bootstrap-datepicker.js
+++ suit/static/suit/js/datepicker/bootstrap-datepicker.js
@@ -1075,6 +1075,9 @@
 			if (!date){
 				this.dates.clear();
 			}
+			if (this.o.multidate === 1 && ix === 0){
+				// single datepicker, don't remove selected date
+			}
 			else if (ix !== -1){
 				this.dates.remove(ix);
 			}
