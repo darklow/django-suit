c4f456e33980255b1c856a73454431b03acfde3f
Kaspars Sprogis <darklow@gmail.com>
2015-04-22 12:40:23 +0300

    Fix datepicker for Firefox when non matching min/sec is set

--- suit/static/suit/js/datepicker/bootstrap-datepicker.js
+++ suit/static/suit/js/datepicker/bootstrap-datepicker.js
@@ -880,8 +880,10 @@
 		_setTimeDropdowns: function(viewDate) {
 			var $timepicker = this.picker.find('.timepicker');
 			$('select[name=hour]', $timepicker).val(('0' + viewDate.getUTCHours()).slice(-2));
-			$('select[name=minute]', $timepicker).val(('0' + viewDate.getUTCMinutes()).slice(-2));
-			$('select[name=second]', $timepicker).val(('0' + viewDate.getUTCSeconds()).slice(-2));
+			var $sel = $('select[name=minute]', $timepicker).val(('0' + viewDate.getUTCMinutes()).slice(-2));
+			$sel.val() === null && $sel.val('00');
+			$sel = $('select[name=second]', $timepicker).val(('0' + viewDate.getUTCSeconds()).slice(-2));
+			$sel.val() === null && $sel.val('00');
 		},
 
 		_getTimeFromDropdowns: function() {
