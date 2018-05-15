664c8ca91db17ff6b4931f997e65befb36115599
Mārtiņš Šulcs <shulcsm@gmail.com>
2018-05-15 10:06:04 +0300

    Fix testsuite.

--- suit/tests/settings.py
+++ suit/tests/settings.py
@@ -14,13 +14,12 @@ INSTALLED_APPS = [
     'django.contrib.sites',
 ]
 
-MIDDLEWARE_CLASSES = [
+MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
--- suit/tests/urls.py
+++ suit/tests/urls.py
@@ -2,5 +2,5 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', admin.site.urls),
 ]
