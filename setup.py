83202e227c32071b0b0909354d2694905572403b
Kaspars Sprogis <darklow@gmail.com>
2019-10-02 10:21:37 +0200

    Improve readme for packaging

--- setup.py
+++ setup.py
@@ -1,9 +1,14 @@
 from setuptools import setup
 
+with open("README.rst", "r") as fh:
+    long_description = fh.read()
+
 setup(
     name='django-suit',
     version=__import__('suit').VERSION,
     description='Modern theme for Django admin interface.',
+    long_description=long_description,
+    long_description_content_type="text/x-rst",
     author='Kaspars Sprogis (darklow)',
     author_email='info@djangosuit.com',
     url='http://djangosuit.com',
